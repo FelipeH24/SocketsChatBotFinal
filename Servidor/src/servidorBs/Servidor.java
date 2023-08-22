d59d00887ac81d971c5634002c578a51bd730cce
Usuario <Usuario@pipe>
2023-08-22 17:40:28 -0500

    declare the following variables:
    Socket socket: Represents the communication socket between the server
    and a client.
    ServerSocket server: Represents the server socket that receives
    connections.
    ObjectInputStream in: to read serialized objects coming from the client.
    int port: Stores the port number.
    and initialize them in the constructor and finally create the run

--- Servidor/src/servidorBs/Servidor.java
+++ Servidor/src/servidorBs/Servidor.java
@@ -1,8 +1,29 @@
 package servidorBs;
 
-public class Servidor {
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Servidor extends Thread {
 	
+	private Socket socket;
+	private ServerSocket server;
+	private ObjectInputStream in;
+	private int port;
+	
+	public Servidor(Socket socket, ServerSocket server, ObjectInputStream in, int port) {
+		super();
+		this.socket = socket;
+		this.server = server;
+		this.in = in;
+		this.port = port;
+	}
+	
+	@Override
+	public void run() {
+		
+		super.run();
+	}
 	
 	public static void main(String[] args) {
 	}
