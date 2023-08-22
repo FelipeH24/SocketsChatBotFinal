065e30b2bf57bb48a6c6b53df8d725ff334922ce
juanc <juanc@192.168.10.106>
2023-08-22 18:32:17 -0500

    Finally, the method was created to start the client and wait for the connection to the server, port 8000.

--- Cliente/src/ClienteBs/Cliente.java
+++ Cliente/src/ClienteBs/Cliente.java
@@ -82,8 +82,8 @@ public class Cliente extends Thread {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cliente cl = new Cliente("127.0.0.1", 8000);
+		cl.start();
 	}
 
 }
