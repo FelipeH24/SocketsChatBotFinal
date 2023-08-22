984c12f8f029e3422e7f9155d66be114dc533b74
Usuario <Usuario@pipe>
2023-08-22 18:22:02 -0500

    Two .txt files were implemented in the code, one with the questions for the bot and another for the answers. In addition, work was done in the run method so that it does the following:
    Read a list of questions from the "questions.txt" file.
    Send each question to the customer through the output stream.
    
    Receives and manages the customer's choice
    Read the customer's choice from the input stream.
    Read a list of responses from the "responses.txt" file.
    Sends the corresponding response to the client based on their choice, or
    a message if the choice is invalid.
    
    close resources
    Closes the input and output streams.
    Closes the ServerSocket object.
    Handle exceptions:
    
    It catches any exception that may occur during the process and prints it
    to the console.

--- Servidor/src/servidorBs/Servidor.java
+++ Servidor/src/servidorBs/Servidor.java
@@ -1,28 +1,94 @@
 package servidorBs;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Servidor extends Thread {
 	
 	private Socket socket;
 	private ServerSocket server;
 	private ObjectInputStream in;
+	private ObjectOutputStream out;
 	private int port;
 	
-	public Servidor(Socket socket, ServerSocket server, ObjectInputStream in, int port) {
+	public Servidor(int port) {
 		super();
-		this.socket = socket;
-		this.server = server;
-		this.in = in;
+		this.socket = null;
+		this.server = null;
+		this.in = null;
+		this.out = null;
 		this.port = port;
 	}
 	
 	@Override
 	public void run() {
+		try {
+			this.server = new ServerSocket(this.port);
+			System.out.println("Inicializando Servidor");
+			System.out.println("Esperando a un Cliente...");
+			this.socket = server.accept();
+			System.out.println(this.socket.getPort());
+			System.out.println("Cliente Aceptado");
+			
+			this.in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+			this.out = new ObjectOutputStream(socket.getOutputStream());
+			
+			List<String> preguntas = new ArrayList<>();
+			try (BufferedReader br = new BufferedReader(new FileReader("preguntas.txt"))){
+				String linea;
+				while ((linea = br.readLine()) != null) {
+					preguntas.add(linea);
+				}		
+			}catch (IOException e) {
+				System.out.println("Error al leer las preguntas");
+				return;
+			}
+			for (String pregunta : preguntas) {
+				this.out.writeUTF(pregunta);
+				this.out.flush();	
+			}
+			this.out.writeUTF("END");
+			this.out.flush();
+			
+			int eleccion = in.readInt();//lee lo que el cliente elije
+			
+			List<String> respuestas = new ArrayList<>();
+			try(BufferedReader br = new BufferedReader(new FileReader("respuestas.txt"))) {
+				String linea;
+				while ((linea = br.readLine()) != null) {
+					respuestas.add(linea);
+				}
+			} catch (IOException e) {
+				System.out.println("Error al leer las respuestas del bot");
+				return;
+			}
+			
+			if (eleccion >= 1 && eleccion <= respuestas.size()) {
+				String respuesta = respuestas.get(eleccion -1);
+				this.out.writeUTF(respuesta);
+				this.out.flush();
+			} else {
+				this.out.writeUTF("No hay respuestas para tu pregunta.");
+				this.out.flush();
+			}
+			this.in.close();
+			this.out.close();
+			this.server.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		
-		super.run();
+
+		
 	}
 	
 	public static void main(String[] args) {
